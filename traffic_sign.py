a38b17c07854ee68ccb96b6837de939df3f909b4
Duy Hung <dongocduyhung7@gmail.com>
2023-04-21 08:46:07 +0700

    train model + done GUI

--- traffic_sign.py
+++ traffic_sign.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 import cv2
-import tensorflow as tfhand
+import tensorflow as tf
 from PIL import Image
 import os
 from sklearn.model_selection import train_test_split
@@ -15,6 +15,7 @@ labels = []
 classes = 43
 cur_path = os.getcwd()
 
+#Retrieving the images and their labels 
 for i in range(classes):
     path = os.path.join(cur_path,'train',str(i))
     images = os.listdir(path)
@@ -30,18 +31,21 @@ for i in range(classes):
         except:
             print("Error loading image")
 
+#Converting lists into numpy arrays
 data = np.array(data)
 labels = np.array(labels)
 
 print(data.shape, labels.shape)
-
+#Splitting training and testing dataset
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 
 print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
+#Converting the labels into one hot encoding
 y_train = to_categorical(y_train, 43)
 y_test = to_categorical(y_test, 43)
 
+#Building the model
 model = Sequential()
 model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=X_train.shape[1:]))
 model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu'))
@@ -56,12 +60,14 @@ model.add(Dense(256, activation='relu'))
 model.add(Dropout(rate=0.5))
 model.add(Dense(43, activation='softmax'))
 
+#Compilation of the model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 epochs = 15
 history = model.fit(X_train, y_train, batch_size=32, epochs=epochs, validation_data=(X_test, y_test))
 model.save("my_model.h5")
 
+#plotting graphs for accur
 plt.figure(0)
 plt.plot(history.history['accuracy'], label='training accuracy')
 plt.plot(history.history['val_accuracy'], label='val accuracy')
@@ -80,6 +86,7 @@ plt.ylabel('loss')
 plt.legend()
 plt.show()
 
+#testing accuracy on test dataset
 from sklearn.metrics import accuracy_score
 
 y_test = pd.read_csv('Test.csv')
@@ -98,5 +105,6 @@ X_test=np.array(data)
 
 pred = model.predict_classes(X_test)
 
+#Accuracy with the test data
 from sklearn.metrics import accuracy_score
 print(accuracy_score(labels, pred))
